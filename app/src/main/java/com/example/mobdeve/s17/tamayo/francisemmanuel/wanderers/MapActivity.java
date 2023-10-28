932bb99c4d37a87bbed8d43fd11238934910fb27
FrancisTamayo <francis_tamayo@dlsu.edu.ph>
2023-10-28 22:54:27 +0800

    map placeholder

--- app/src/main/java/com/example/mobdeve/s17/tamayo/francisemmanuel/wanderers/MapActivity.java
+++ app/src/main/java/com/example/mobdeve/s17/tamayo/francisemmanuel/wanderers/MapActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobdeve.s17.tamayo.francisemmanuel.wanderers;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,21 +13,34 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity {
+    ImageView imgMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        /*
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapGoogle);
         mapFragment.getMapAsync(this);
+
+         */
+
+        imgMap = (ImageView) findViewById(R.id.imgMap);
     }
 
+    /*
     @Override
     public void onMapReady(GoogleMap googleMap) {
         googleMap.addMarker(new MarkerOptions()
                 .position(new LatLng(0, 0))
                 .title("Marker"));
+    }*/
+    public void nextAct(View v) {
+        Intent nextIntent = new Intent(this, JournalActivity.class);
+
+        this.startActivity(nextIntent);
+        this.finish();
     }
 }
