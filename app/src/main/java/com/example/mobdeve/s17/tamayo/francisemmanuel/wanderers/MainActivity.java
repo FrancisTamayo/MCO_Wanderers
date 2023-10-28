72c2d1b648a61396c3db86101124bf79b09253b2
FrancisTamayo <francis_tamayo@dlsu.edu.ph>
2023-10-28 15:44:48 +0800

    initial commit

---
+++ app/src/main/java/com/example/mobdeve/s17/tamayo/francisemmanuel/wanderers/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobdeve.s17.tamayo.francisemmanuel.wanderers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
